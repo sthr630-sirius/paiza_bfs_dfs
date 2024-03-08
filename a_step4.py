754114a42aa5bf0e053218510439f32fd152462b
sthr630-sirius <100351188+sthr630-sirius@users.noreply.github.com>
2024-03-08 15:09:30 +0900

    a_step2-6

---
+++ a_step4.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+def withinField(h, w, y, x):
+    if 0<= y <h and 0<= x <w:
+        return True
+    else:
+        return False
+
+def underMaxStep(max_step, step):
+    if step < max_step:
+        return True
+    else:
+        return False
+
+def isNotWall(field, y, x):
+    if field[y][x] != "#":
+        return True
+    else:
+        return False
+
+h, w, n = map(int, input().split())
+y, x = map(int, input().split())
+
+delta = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+
+max_step = n
+
+field = [["."]*w for _ in range(h)]
+for i in range(h):
+    field[i] = list(input())
+
+visited = [[False]*w for _ in range(h)]
+next_p = deque()
+
+"""
+p = [y, x, step]
+"""
+next_p.append([y, x, 0])
+field[y][x] = "*"
+visited[y][ x] = True
+
+while next_p:
+    np = next_p.popleft()
+    y = np[0]
+    x = np[1]
+    step = np[2]
+
+    for dy, dx in delta:
+        if withinField(h, w, y+dy, x+dx) and not visited[y+dy][x+dx] and underMaxStep(max_step, step) and isNotWall(field, y+dy, x+dx):
+            next_p.append([y+dy, x+dx, step+1])
+            field[y+dy][x+dx] = "*"
+            visited[y+dy][x+dx] = True
+
+for i in range(h):
+    print(*field[i], sep="")
+
---
+++ a_step6.py
@@ -0,0 +1,66 @@
+from collections import deque
+
+def withinField(h, w, y, x):
+    if 0<= y <h and 0<= x <w:
+        return True
+    else:
+        return False
+
+def underMaxStep(max_step, step):
+    if step < max_step:
+        return True
+    else:
+        return False
+
+def isNotWall(field, y, x):
+    if field[y][x] != "#":
+        return True
+    else:
+        return False
+
+h, w = map(int, input().split())
+y, x = map(int, input().split())
+gy, gx = map(int, input().split())
+
+delta = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+
+#max_step = n
+ans_step = 0
+
+field = [["."]*w for _ in range(h)]
+for i in range(h):
+    field[i] = list(input())
+
+visited = [[False]*w for _ in range(h)]
+next_p = deque()
+
+"""
+p = [y, x, step]
+"""
+next_p.append([y, x, 0])
+field[y][x] = "*"
+visited[y][ x] = True
+
+while next_p:
+    np = next_p.popleft()
+    y = np[0]
+    x = np[1]
+    step = np[2]
+
+    for dy, dx in delta:
+        if withinField(h, w, y+dy, x+dx) and not visited[y+dy][x+dx] and isNotWall(field, y+dy, x+dx):
+            next_p.append([y+dy, x+dx, step+1])
+            field[y+dy][x+dx] = "*"
+            visited[y+dy][x+dx] = True
+
+            if y+dy == gy and x+dx == gx:
+                ans_step = step+1
+
+if field[gy][gx] == "*":
+    print(ans_step)
+else:
+    print("No")
+
+#for i in range(h):
+#    print(*field[i], sep="")
+
